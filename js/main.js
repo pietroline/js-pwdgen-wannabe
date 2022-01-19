5dfd7280da437fcf46d57c873c5271f8bbf125d3
pietroline <pietrobarberi@hotmail.it>
2022-01-19 17:30:26 +0100

    html e css

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 // Chiedo all'utente di inserire un nome
 let nome = prompt('Inserisci il tuo nome');
+document.getElementById("nome").innerHTML = nome;
 console.log(`Il nome inserito è: ${nome}`);
 
 // Chiedo all'utente di inserire un cognome
 let cognome = prompt('Inserisci il tuo cognome');
+document.getElementById("cognome").innerHTML = cognome;
 console.log(`Il cognome inserito è: ${cognome}`);
 
 // Chiedo all'utente di inserire il colore preferito
 let colore = prompt('Inserisci il tuo colore preferito');
+document.getElementById("colore").innerHTML = colore;
 console.log(`Il colore preferito inserito è: ${colore}`);
 
 // Genero password
